7b4f25effe50b9b65c4637fb64c8b5ade72bbf1e
nburak <nburak@fnc-group.ru>
2025-09-03 19:41:44 +0300

    add docker profile, routing for auth-server

--- src/main/java/guru/springframework/spring6gateway/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6gateway/config/SpringSecurityConfig.java
@@ -24,7 +24,10 @@ public class SpringSecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/oauth2/**")
+                        .permitAll()
+                        .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
